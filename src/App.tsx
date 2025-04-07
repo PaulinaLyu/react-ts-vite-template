c762958b2bb7802e8ca57925e083ec1249424064
lyubeznova.polina <lyubeznova.polina@geogracom.com>
2025-04-07 15:57:48 +0300

    lint-staged test4

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,9 @@ function App() {
       <div>React TS Vite Template</div>
       <Test />
       <div>Test</div>
-      <div>Test2</div>
+
       <div>Test</div>
       <div>Test2</div>
-      <div>Test</div>
-      <div>Test2</div>
-                 <div>Test</div>
-                   <div>Test2</div>
     </>
   );
 }
